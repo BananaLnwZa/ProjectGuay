efbcc6e667da41942e9744898fb901a02fab2ecf
GuayNoi <ratlita13@gmail.com>
2025-07-31 11:50:57 +0700

    Mobile demo

---
+++ frontend/mobile/lib/src/account.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatefulWidget {
+  const AccountPage({Key? key}) : super(key: key);
+
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Account Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Welcome to your account!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
