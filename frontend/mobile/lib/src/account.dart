03722894f898f77ae102a1fead68adc17da94055
BananaLnwZa <ratlita13@gmail.com>
2025-08-03 00:30:09 +0700

    config

--- frontend/mobile/lib/src/account.dart
+++ frontend/mobile/lib/src/account.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatefulWidget {
-  const AccountPage({Key? key}) : super(key: key);
+  const AccountPage({super.key});
 
   @override
   _AccountPageState createState() => _AccountPageState();
